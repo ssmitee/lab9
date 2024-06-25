870a37316f47bd6f4e3dd2b6344121c8158f20f2
rinterron <144239782+ssmitee@users.noreply.github.com>
2024-06-26 00:37:19 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем словарь для хранения информации о цветах
+        var colors = new Dictionary<string, Dictionary<string, string>>
+        {
+            {
+                "черный", new Dictionary<string, string>
+                {
+                    {"name", "black"},
+                    {"rgb10", "rgb(0, 0, 0)"},
+                    {"rgb16", "#000000"}
+                }
+            },
+            {
+                "серый", new Dictionary<string, string>
+                {
+                    {"name", "grey"},
+                    {"rgb10", "rgb(128, 128, 128)"},
+                    {"rgb16", "#808080"}
+                }
+            },
+            {
+                "белый", new Dictionary<string, string>
+                {
+                    {"name", "white"},
+                    {"rgb10", "rgb(255, 255, 255)"},
+                    {"rgb16", "#FFFFFF"}
+                }
+            }
+        };
+
+        // Получаем представление серого цвета
+        var greyColor = colors["серый"];
+
+        // Выводим представление серого цвета
+        Console.WriteLine($"color: {greyColor["name"]};");
+        Console.WriteLine($"color: {greyColor["rgb10"]};");
+        Console.WriteLine($"color: {greyColor["rgb16"]};");
+    }
+}
